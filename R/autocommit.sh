714fd294e52d392b919899774137771cbba25b58
Charles Santana <charles@aifinancegroup.com>
2020-03-18 00:50:42 +0100

    Adding auto-commit

---
+++ R/autocommit.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+sufix=$1
+bz2filename=$2
+
+msg="Adding file $sufix"
+git add "../data/$bz2filename"
+git commit -m "$msg"
+git remote set-url origin git@github.com:dadoscope/covid19.git
+git push origin master
