0ce8ed1e9bc4a8942d462986dcf0293a646fe51b
cndesantana <charles.santana@gmail.com>
2020-03-18 06:20:46 +0000

    Adding file 202003180215

--- R/autocommit.sh
+++ R/autocommit.sh
@@ -4,7 +4,7 @@ sufix=$1
 bz2filename=$2
 
 msg="Adding file $sufix"
-git add "../data/$bz2filename"
+git add "$bz2filename"
 git commit -am "$msg"
 git remote set-url origin git@github.com:dadoscope/covid19.git
 git push origin master
--- R/raspador.R
+++ R/raspador.R
@@ -2,7 +2,7 @@ library(rtweet)
 library(tidyverse)
 library(jsonlite)
 
-wd <- "/home/ubuntu/raspador/covid19/R/"
+basedir<- "/home/ubuntu/raspador/covid19/R/"
 terms <- c("covid","covid-19","corona","coronavirus")
 all_tweets <- data.frame()
 sufix <- system("date +'%Y%m%d%H%M'", intern=TRUE)
@@ -12,8 +12,8 @@ for(t in terms){
    tweets$term <- t
    all_tweets <-rbind(all_tweets, tweets)
 }
-jsonfilename <- paste0(basedir,"data/tweets_",sufix,".json")
-bz2filename <- paste0(basedir,"data/tweets_",sufix,".bz2")
+jsonfilename <- paste0(basedir,"../data/tweets_",sufix,".json")
+bz2filename <- paste0(basedir,"../data/tweets_",sufix,".bz2")
 write_json(all_tweets, jsonfilename, pretty = TRUE, auto_unbox = FALSE)
 system(paste("tar -cvjf",bz2filename,jsonfilename))
 system(paste("sh /home/ubuntu/raspador/covid19/R/autocommit.sh",sufix,bz2filename))
