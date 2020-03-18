f37fc2b609b1f2e99596f31cd895f6344995a4c7
cndesantana <charles.santana@gmail.com>
2020-03-18 00:13:36 +0000

    update

--- R/autocommit.sh
+++ R/autocommit.sh
@@ -5,6 +5,6 @@ bz2filename=$2
 
 msg="Adding file $sufix"
 git add "../data/$bz2filename"
-git commit -m "$msg"
+git commit -am "$msg"
 git remote set-url origin git@github.com:dadoscope/covid19.git
 git push origin master
